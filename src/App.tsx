7fc9262298cdc12f75bc0dbc902cdfd26b9c6ba9
Daryl Tng <42925677+mrdarylguy@users.noreply.github.com>
2023-09-26 00:21:26 +0800

    Import syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { Home } from "./pages/home";
+import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
-import { Navbar } from "./components/NavBar";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
